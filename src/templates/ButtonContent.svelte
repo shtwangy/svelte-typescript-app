895c12fe2ca634615e1fb257da1a060c1b88da01
shtwangy <shtwangy@gmail.com>
2021-02-02 08:59:25 +0900

    create Button component

---
+++ src/templates/ButtonContent.svelte
@@ -0,0 +1,18 @@
+<div class="button-content">
+    <button class="button">Copy</button>
+</div>
+
+<style>
+.button-content {
+    width: 100%;
+    text-align: right;
+}
+.button {
+    margin: 16px 8px 0 0;
+    padding: 8px 16px;
+    background-color: #dee7e6;
+    color: #002227;
+    border: none;
+    border-radius: 4px;
+}
+</style>
