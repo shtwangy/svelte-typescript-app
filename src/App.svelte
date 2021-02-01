d5ac85396e9eb57ac415942dff6490a961ed00e6
shtwangy <shtwangy@gmail.com>
2021-02-02 08:31:20 +0900

    create Content component

--- src/App.svelte
+++ src/App.svelte
@@ -1,28 +1,11 @@
 <script lang="ts">
     import Title from './templates/Title.svelte';
-    let url: string | undefined = '';
-    let title: string | undefined = '';
-    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-        const currentTab = tabs[0];
-        url = currentTab.url;
-        title = currentTab.title;
-    });
+    import Content from './templates/Content.svelte';
 </script>
 
 <div class="container">
     <Title />
-    <div>
-        <h2>Title</h2>
-        <p>{title}</p>
-    </div>
-    <div>
-        <h2>URL</h2>
-        <p>{url}</p>
-    </div>
-    <div>
-        <h2>Text to be copied</h2>
-        <p>{title}<br/>{url}</p>
-    </div>
+    <Content />
 </div>
 
 <style>
---
+++ src/templates/Content.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    let url: string | undefined = '';
+    let title: string | undefined = '';
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+        const currentTab = tabs[0];
+        url = currentTab.url;
+        title = currentTab.title;
+    });
+</script>
+
+<div>
+    <h2>Title</h2>
+    <p>{title}</p>
+</div>
+<div>
+    <h2>URL</h2>
+    <p>{url}</p>
+</div>
+<div>
+    <h2>Text to be copied</h2>
+    <p>{title}<br/>{url}</p>
+</div>
+
+<style>
+    .title {
+        text-align: center;
+        font-size: 12px;
+        background-color: #c71585;
+        margin: 0;
+        padding: 2px;
+    }
+</style>
